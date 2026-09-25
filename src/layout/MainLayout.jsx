b4c988a88a3fb3b4e8e4d8fd832351fb8292addf
rupeshlalkumar <rupeshkumar2575135@gmail.com>
2026-09-25 08:19:48 +0530

    Day 10 (Morning) : Fix Scroll Navigation Bug.
    Day 10 (Morning) : Fix Scroll Navigation Bug.

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 function MainLayout() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
 
       <main>
